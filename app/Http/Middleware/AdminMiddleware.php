6c5ee9dce6310f0064d6ea09a5d077b1877ae186
BreinerSan <breiner.sv@gmail.com>
2024-08-21 23:24:04 -0500

    Creado middleware para rol administrador

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->role === 'Administrador') {
+            return $next($request);
+        }
+        
+        return redirect('/principal')->with('error', 'No tienes permiso para acceder a esta sección.');
+    }
+}
